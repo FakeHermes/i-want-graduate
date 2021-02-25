2d9325cacb83f9be767004f824fbec02f1559d44
FakeHermes <492141730@qq.com>
2021-02-26 01:50:04 +0800

    playback

--- salsify-sender.cc
+++ salsify-sender.cc
@@ -396,130 +396,154 @@ int main(int argc, char *argv[])
   Poller poller;
 
   //audio para set
-	int err;
-	char *buffer;
-	int buffer_frames = 1024;
-	unsigned int rate = AUDIO_RATE_SET;
-	snd_pcm_t *capture_handle;// 一个指向PCM设备的句柄
-	snd_pcm_hw_params_t *hw_params; //此结构包含有关硬件的信息，可用于指定PCM流的配置
+	// int err;
+	// char *buffer;
+	// int buffer_frames = 1024;
+	// unsigned int rate = AUDIO_RATE_SET;
+	// snd_pcm_t *capture_handle;// 一个指向PCM设备的句柄
+	// snd_pcm_hw_params_t *hw_params; //此结构包含有关硬件的信息，可用于指定PCM流的配置
 	
-	/*注册信号捕获退出接口*/
-	signal(2,exit_sighandler);
+	// /*注册信号捕获退出接口*/
+	// signal(2,exit_sighandler);
  
-	/*PCM的采样格式在pcm.h文件里有定义*/
-	snd_pcm_format_t format=AudioFormat; // 采样位数：16bit、LE格式
+	// /*PCM的采样格式在pcm.h文件里有定义*/
+	// snd_pcm_format_t format=AudioFormat; // 采样位数：16bit、LE格式
  
-	/*打开音频采集卡硬件，并判断硬件是否打开成功，若打开失败则打印出错误提示*/
-	// SND_PCM_STREAM_PLAYBACK 输出流
-	// SND_PCM_STREAM_CAPTURE  输入流
-	if ((err = snd_pcm_open (&capture_handle, argv[optind+4] ,SND_PCM_STREAM_CAPTURE,0))<0) 
-	{
-		printf("无法打开音频设备: %s (%s)\n",  argv[optind+4],snd_strerror (err));
-		exit(1);
-	}
-	printf("音频接口打开成功.\n");
+	// /*打开音频采集卡硬件，并判断硬件是否打开成功，若打开失败则打印出错误提示*/
+	// // SND_PCM_STREAM_PLAYBACK 输出流
+	// // SND_PCM_STREAM_CAPTURE  输入流
+	// if ((err = snd_pcm_open (&capture_handle, argv[optind+4] ,SND_PCM_STREAM_CAPTURE,0))<0) 
+	// {
+	// 	printf("无法打开音频设备: %s (%s)\n",  argv[optind+4],snd_strerror (err));
+	// 	exit(1);
+	// }
+	// printf("音频接口打开成功.\n");
  
-	/*分配硬件参数结构对象，并判断是否分配成功*/
-	if((err = snd_pcm_hw_params_malloc(&hw_params)) < 0) 
-	{
-		printf("无法分配硬件参数结构 (%s)\n",snd_strerror(err));
-		exit(1);
-	}
-	printf("硬件参数结构已分配成功.\n");
+	// /*分配硬件参数结构对象，并判断是否分配成功*/
+	// if((err = snd_pcm_hw_params_malloc(&hw_params)) < 0) 
+	// {
+	// 	printf("无法分配硬件参数结构 (%s)\n",snd_strerror(err));
+	// 	exit(1);
+	// }
+	// printf("硬件参数结构已分配成功.\n");
 	
-	/*按照默认设置对硬件对象进行设置，并判断是否设置成功*/
-	if((err=snd_pcm_hw_params_any(capture_handle,hw_params)) < 0) 
-	{
-		printf("无法初始化硬件参数结构 (%s)\n", snd_strerror(err));
-		exit(1);
-	}
-	printf("硬件参数结构初始化成功.\n");
+	// /*按照默认设置对硬件对象进行设置，并判断是否设置成功*/
+	// if((err=snd_pcm_hw_params_any(capture_handle,hw_params)) < 0) 
+	// {
+	// 	printf("无法初始化硬件参数结构 (%s)\n", snd_strerror(err));
+	// 	exit(1);
+	// }
+	// printf("硬件参数结构初始化成功.\n");
  
-	/*
-		设置数据为交叉模式，并判断是否设置成功
-		interleaved/non interleaved:交叉/非交叉模式。
-		表示在多声道数据传输的过程中是采样交叉的模式还是非交叉的模式。
-		对多声道数据，如果采样交叉模式，使用一块buffer即可，其中各声道的数据交叉传输；
-		如果使用非交叉模式，需要为各声道分别分配一个buffer，各声道数据分别传输。
-	*/
-	if((err = snd_pcm_hw_params_set_access (capture_handle,hw_params,SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) 
-	{
-		printf("无法设置访问类型(%s)\n",snd_strerror(err));
-		exit(1);
-	}
-	printf("访问类型设置成功.\n");
+	// /*
+	// 	设置数据为交叉模式，并判断是否设置成功
+	// 	interleaved/non interleaved:交叉/非交叉模式。
+	// 	表示在多声道数据传输的过程中是采样交叉的模式还是非交叉的模式。
+	// 	对多声道数据，如果采样交叉模式，使用一块buffer即可，其中各声道的数据交叉传输；
+	// 	如果使用非交叉模式，需要为各声道分别分配一个buffer，各声道数据分别传输。
+	// */
+	// if((err = snd_pcm_hw_params_set_access (capture_handle,hw_params,SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) 
+	// {
+	// 	printf("无法设置访问类型(%s)\n",snd_strerror(err));
+	// 	exit(1);
+	// }
+	// printf("访问类型设置成功.\n");
  
-	/*设置数据编码格式，并判断是否设置成功*/
-	if ((err=snd_pcm_hw_params_set_format(capture_handle, hw_params,format)) < 0) 
-	{
-		printf("无法设置格式 (%s)\n",snd_strerror(err));
-		exit(1);
-	}
-	fprintf(stdout, "PCM数据格式设置成功.\n");
+	// /*设置数据编码格式，并判断是否设置成功*/
+	// if ((err=snd_pcm_hw_params_set_format(capture_handle, hw_params,format)) < 0) 
+	// {
+	// 	printf("无法设置格式 (%s)\n",snd_strerror(err));
+	// 	exit(1);
+	// }
+	// fprintf(stdout, "PCM数据格式设置成功.\n");
  
-	/*设置采样频率，并判断是否设置成功*/
-	if((err=snd_pcm_hw_params_set_rate_near(capture_handle,hw_params,&rate,0))<0) 
-	{
-		printf("无法设置采样率(%s)\n",snd_strerror(err));
-		exit(1);
-	}
-	printf("采样率设置成功\n");
+	// /*设置采样频率，并判断是否设置成功*/
+	// if((err=snd_pcm_hw_params_set_rate_near(capture_handle,hw_params,&rate,0))<0) 
+	// {
+	// 	printf("无法设置采样率(%s)\n",snd_strerror(err));
+	// 	exit(1);
+	// }
+	// printf("采样率设置成功\n");
  
-	/*设置声道，并判断是否设置成功*/
-	if((err = snd_pcm_hw_params_set_channels(capture_handle, hw_params,AUDIO_CHANNEL_SET)) < 0) 
-	{
-		printf("无法设置声道数(%s)\n",snd_strerror(err));
-		exit(1);
-	}
-	printf("声道数设置成功.\n");
+	// /*设置声道，并判断是否设置成功*/
+	// if((err = snd_pcm_hw_params_set_channels(capture_handle, hw_params,AUDIO_CHANNEL_SET)) < 0) 
+	// {
+	// 	printf("无法设置声道数(%s)\n",snd_strerror(err));
+	// 	exit(1);
+	// }
+	// printf("声道数设置成功.\n");
  
-	/*将配置写入驱动程序中，并判断是否配置成功*/
-	if ((err=snd_pcm_hw_params (capture_handle,hw_params))<0) 
-	{
-		printf("无法向驱动程序设置参数(%s)\n",snd_strerror(err));
-		exit(1);
-	}
-	printf("参数设置成功.\n");
+	// /*将配置写入驱动程序中，并判断是否配置成功*/
+	// if ((err=snd_pcm_hw_params (capture_handle,hw_params))<0) 
+	// {
+	// 	printf("无法向驱动程序设置参数(%s)\n",snd_strerror(err));
+	// 	exit(1);
+	// }
+	// printf("参数设置成功.\n");
  
-	/*使采集卡处于空闲状态*/
-	snd_pcm_hw_params_free(hw_params);
+	// /*使采集卡处于空闲状态*/
+	// snd_pcm_hw_params_free(hw_params);
  
-	/*准备音频接口,并判断是否准备好*/
-	if((err=snd_pcm_prepare(capture_handle))<0) 
-	{
-		printf("无法使用音频接口 (%s)\n",snd_strerror(err));
-		exit(1);
-	}
-	printf("音频接口准备好.\n");
+	// /*准备音频接口,并判断是否准备好*/
+	// if((err=snd_pcm_prepare(capture_handle))<0) 
+	// {
+	// 	printf("无法使用音频接口 (%s)\n",snd_strerror(err));
+	// 	exit(1);
+	// }
+	// printf("音频接口准备好.\n");
  
-	/*配置一个数据缓冲区用来缓冲数据*/
-	//snd_pcm_format_width(format) 获取样本格式对应的大小(单位是:bit)
-	int frame_byte=snd_pcm_format_width(format)/8;
-	buffer=(char *)malloc(buffer_frames*frame_byte*AUDIO_CHANNEL_SET); //2048
-	printf("缓冲区分配成功.\n");
+	// /*配置一个数据缓冲区用来缓冲数据*/
+	// //snd_pcm_format_width(format) 获取样本格式对应的大小(单位是:bit)
+	// int frame_byte=snd_pcm_format_width(format)/8;
+	// buffer=(char *)malloc(buffer_frames*frame_byte*AUDIO_CHANNEL_SET); //2048
+	// printf("缓冲区分配成功.\n");
   //audio para set end
 
   //从声卡采集数据
+  // poller.add_action(Poller::Action(
+  //   udpsocket, Direction::Out, [&](){
+      
+  //     while(true){
+  //         if((err=snd_pcm_readi(capture_handle,buffer,buffer_frames))!=buffer_frames) {
+  //           printf("从音频接口读取失败(%s)\n",snd_strerror(err));
+  //           exit(1);
+  //         }
+  //         //
+  //         string audio_frame(buffer);
+
+  //         udpsocket2.send(audio_frame); //发送音频数据
+  //     }
+      
+
+  //     return ResultType::Continue;
+  //   }
+  // ));
+
+  //从文件读取音频
   poller.add_action(Poller::Action(
     udpsocket, Direction::Out, [&](){
       
-      while(true){
-          if((err=snd_pcm_readi(capture_handle,buffer,buffer_frames))!=buffer_frames) {
-            printf("从音频接口读取失败(%s)\n",snd_strerror(err));
-            exit(1);
-          }
-          //
-          string audio_frame(buffer);
+      FILE *ts_file = fopen(argv[optind+4], "r+"); 
+        char h[44];
+        char *buff;
+        buff=(char *)malloc(1024);
+        int len __attribute__((unused)) =1;
+	     len=fread(h,1,44,ts_file); //44numofcount:0,去除wav header
+	    while(!feof(ts_file)){  
+        len=fread(buff, 1, 1024, ts_file);  
 
-          udpsocket2.send(audio_frame);
-      }
+        string audio_frame(buff);
+        
+        udpsocket2.send(audio_frame);//发送音频数据
+        usleep(10000);  
+	    } 
       
 
       return ResultType::Continue;
     }
   ));
 
-  //发送音频数据
+ 
 
   /* fetch frames from webcam */
   poller.add_action(Poller::Action(encode_start_pipe.second, Direction::In,
