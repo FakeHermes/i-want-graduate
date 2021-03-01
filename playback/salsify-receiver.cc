7bea3c332e875a2c6100b765db2fc0df4c00fac8
virtual <492141730@qq.com>
2021-03-01 22:43:49 +0800

    audio and video simutaneously

---
+++ playback/salsify-receiver.cc
@@ -0,0 +1,577 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+/* Copyright 2013-2018 the Alfalfa authors
+                       and the Massachusetts Institute of Technology
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+
+      1. Redistributions of source code must retain the above copyright
+         notice, this list of conditions and the following disclaimer.
+
+      2. Redistributions in binary form must reproduce the above copyright
+         notice, this list of conditions and the following disclaimer in the
+         documentation and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+#include <getopt.h>
+
+#include <cstdlib>
+#include <random>
+#include <unordered_map>
+#include <utility>
+#include <tuple>
+#include <queue>
+#include <deque>
+#include <thread>
+#include <condition_variable>
+#include <future>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/file.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <regex.h>
+#include <alsa/asoundlib.h>
+
+#include "socket.hh"
+#include "packet.hh"
+#include "poller.hh"
+#include "optional.hh"
+#include "player.hh"
+#include "display.hh"
+#include "paranoid.hh"
+#include "procinfo.hh"
+
+#define PCM_DEVICE "default"
+#define TS_PACKET_SIZE 188
+#define MTU 1500
+
+using namespace std;
+using namespace std::chrono;
+using namespace PollerShortNames;
+FILE *pcm_data_file=NULL;
+
+class AverageInterPacketDelay
+{
+private:
+  static constexpr double ALPHA = 0.1;
+
+  double value_{-1.0};
+  uint64_t last_update_{0};
+
+public:
+  void add(const uint64_t timestamp_us, const int32_t grace)
+  {
+    assert(timestamp_us >= last_update_);
+
+    if (value_ < 0)
+    {
+      value_ = 0;
+    }
+    // else if ( timestamp_us - last_update_ > 0.2 * 1000 * 1000 /* 0.2 seconds */ ) {
+    //   value_ /= 4;
+    // }
+    else
+    {
+      double new_value = max(0l, static_cast<int64_t>(timestamp_us - last_update_ - grace));
+      value_ = ALPHA * new_value + (1 - ALPHA) * value_;
+    }
+
+    last_update_ = timestamp_us;
+  }
+
+  uint32_t int_value() const { return static_cast<uint32_t>(value_); }
+};
+
+void usage(const char *argv0)
+{
+  cerr << "Usage: " << argv0 << " [-f, --fullscreen] [--verbose] PORT WIDTH HEIGHT" << endl;
+}
+
+uint16_t ezrand()
+{
+  random_device rd;
+  uniform_int_distribution<uint16_t> ud;
+
+  return ud(rd);
+}
+
+queue<RasterHandle> display_queue;
+queue<string> audio_queue;
+mutex mtx;
+condition_variable cv;
+
+void display_task(const VP8Raster &example_raster, bool fullscreen)
+{
+  VideoDisplay display{example_raster, fullscreen};
+
+  while (true)
+  {
+    unique_lock<mutex> lock(mtx);
+    cv.wait(lock, []() { return not display_queue.empty(); });
+
+    while (not display_queue.empty())
+    {
+      display.draw(display_queue.front());
+      display_queue.pop();
+    }
+  }
+}
+
+void audio_task(int &pcm,snd_pcm_t *pcm_handle,snd_pcm_uframes_t frames,unsigned int &tmp,int &seconds,int &buff_size)
+{
+  
+  
+  /*audo para set end*/
+  /*while (true)
+  {
+    unique_lock<mutex> lock(mtx);
+    cv.wait(lock, []() { return not audio_queue.empty(); });
+
+    while (not audio_queue.empty())
+    {
+      string pcm_payload=audio_queue.front();
+      char * recv_buff = new char [pcm_payload.length()+1];
+      strcpy (recv_buff, pcm_payload.c_str());
+     // printf("%s\n",recv_buff);
+      
+    
+		  
+      if ((pcm = snd_pcm_writei(pcm_handle, recv_buff, frames) == -EPIPE)){
+          printf("XRUN.\n");
+          snd_pcm_prepare(pcm_handle);
+      }
+      audio_queue.pop();
+      
+    }
+  }*/
+  int loops;
+  char *buff;
+  buff = (char *) malloc(buff_size);
+  for (loops = (seconds * 1000000) / tmp; loops > 0; loops--) {
+
+            if ((pcm = read(0, buff, buff_size) == 0)) {
+              printf("Early end of file.\n");
+              return ;
+            }
+
+            if ((pcm = snd_pcm_writei(pcm_handle, buff, frames) == -EPIPE)) {
+			        printf("XRUN.\n");
+			        snd_pcm_prepare(pcm_handle);
+		        } else if (pcm < 0) {
+			        printf("ERROR. Can't write to PCM device. %s\n", snd_strerror(pcm));
+		        }
+           // printf("%s\n",buff);
+           
+            cerr << "send audio." << endl;
+
+          }
+
+          
+          free(buff);
+          return ;
+}
+
+void enqueue_frame(FramePlayer &player, const Chunk &frame)
+{
+  if (frame.size() == 0)
+  {
+    return;
+  }
+
+  const Optional<RasterHandle> raster = player.decode(frame);
+
+  async(launch::async,
+        [&raster]() {
+          if (raster.initialized())
+          {
+            lock_guard<mutex> lock(mtx);
+            display_queue.push(raster.get());
+            cv.notify_all();
+          }
+        });
+}
+
+
+int main(int argc, char *argv[])
+{
+  // argc:count of arguments
+  /* check the command-line arguments */
+  if (argc < 1)
+  { /* for sticklers */
+    abort();
+  }
+
+  /* fullscreen player */
+  bool fullscreen = false;
+  bool verbose = false;
+  printf("%d",int(fullscreen));
+  const option command_line_options[] = {
+      {"fullscreen", no_argument, nullptr, 'f'},
+      {"verbose", no_argument, nullptr, 'v'},
+      {0, 0, 0, 0}};
+
+  while (true)
+  {
+    const int opt = getopt_long(argc, argv, "f", command_line_options, nullptr);
+
+    if (opt == -1)
+    {
+      //命令行打印命令?
+      cerr << "opt: " << opt << endl;
+      break;
+    }
+
+    switch (opt)
+    {
+    case 'f':
+      fullscreen = true;
+      break;
+
+    case 'v':
+      verbose = true;
+      break;
+
+    default:
+      usage(argv[0]);
+      return EXIT_FAILURE;
+    }
+  }
+
+  if (optind + 2 >= argc)
+  {
+    usage(argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  /* choose a random connection_id */
+  const uint16_t connection_id = 1337; // ezrand();
+  cerr << "Connection ID: " << connection_id << endl;
+
+  /* construct Socket for incoming  datagrams */
+  UDPSocket udpsocket;
+  UDPSocket udpsocket2;
+  udpsocket.bind(Address("0", argv[optind]));
+  udpsocket.set_timestamps();
+
+  udpsocket2.bind(Address("0", argv[optind+3]));
+  udpsocket2.set_timestamps();
+  
+  //audio 
+/* audio para*/
+  unsigned int tmp ,rate=8000;
+	int pcm,channels=2, seconds=10;
+	snd_pcm_t *pcm_handle;
+	snd_pcm_hw_params_t *params;
+	snd_pcm_uframes_t frames;
+	
+	int buff_size;
+
+  /* Open the PCM device in playback mode */
+	if ((pcm = snd_pcm_open(&pcm_handle, PCM_DEVICE,
+					SND_PCM_STREAM_PLAYBACK, 0) < 0))
+		printf("ERROR: Can't open \"%s\" PCM device. %s\n",
+					PCM_DEVICE, snd_strerror(pcm));
+
+	/* Allocate parameters object and fill it with default values*/
+	snd_pcm_hw_params_alloca(&params);
+
+	snd_pcm_hw_params_any(pcm_handle, params);
+
+	/* Set parameters */
+	if ((pcm = snd_pcm_hw_params_set_access(pcm_handle, params,
+					SND_PCM_ACCESS_RW_INTERLEAVED) < 0))
+		printf("ERROR: Can't set interleaved mode. %s\n", snd_strerror(pcm));
+
+	if ((pcm = snd_pcm_hw_params_set_format(pcm_handle, params,
+						SND_PCM_FORMAT_S16_LE) < 0)) 
+		printf("ERROR: Can't set format. %s\n", snd_strerror(pcm));
+
+	if ((pcm = snd_pcm_hw_params_set_channels(pcm_handle, params, channels) < 0)) 
+		printf("ERROR: Can't set channels number. %s\n", snd_strerror(pcm));
+
+	if ((pcm = snd_pcm_hw_params_set_rate_near(pcm_handle, params, &rate, 0) < 0))
+		printf("ERROR: Can't set rate. %s\n", snd_strerror(pcm));
+
+	/* Write parameters */
+	if ((pcm = snd_pcm_hw_params(pcm_handle, params) < 0))
+		printf("ERROR: Can't set harware parameters. %s\n", snd_strerror(pcm));
+
+	/* Resume information */
+	printf("PCM name: '%s'\n", snd_pcm_name(pcm_handle));
+
+	printf("PCM state: %s\n", snd_pcm_state_name(snd_pcm_state(pcm_handle)));
+
+	snd_pcm_hw_params_get_channels(params, &tmp);
+	printf("channels: %i ", tmp);
+
+	if (tmp == 1)
+		printf("(mono)\n");
+	else if (tmp == 2)
+		printf("(stereo)\n");
+
+	snd_pcm_hw_params_get_rate(params, &tmp, 0);
+	printf("rate: %d bps\n", tmp);
+
+	printf("seconds: %d\n", seconds);	
+
+	/* Allocate buffer to hold single period */
+	snd_pcm_hw_params_get_period_size(params, &frames, 0);
+
+	buff_size = frames * channels * 2 /* 2 -> sample size */;
+	
+	snd_pcm_hw_params_get_period_time(params, &tmp, NULL);
+
+  /*创建一个保存PCM数据的文件*/
+	if((pcm_data_file = fopen("m1.wav", "wb")) == NULL)
+	{
+		printf("无法创建%s音频文件.\n",argv[2]);
+		exit(1);
+	} 
+	printf("用于录制的音频文件已打开.\n");
+  //audio_end
+  /* construct FramePlayer */
+  FramePlayer player(paranoid::stoul(argv[optind + 1]), paranoid::stoul(argv[optind + 2]));
+  player.set_error_concealment(true);
+
+  /* construct display thread */
+  thread([&player, fullscreen]() { display_task(player.example_raster(), fullscreen); }).detach();
+
+  /*audio play thread*/
+  thread([&]() { audio_task(pcm,pcm_handle,frames,tmp,seconds,buff_size); }).detach();
+
+  /* frame no => FragmentedFrame; used when receiving packets out of order */
+  unordered_map<size_t, FragmentedFrame> fragmented_frames;
+  size_t next_frame_no = 0;
+
+  /* EWMA */
+  AverageInterPacketDelay avg_delay;
+
+  /* decoder states */
+  uint32_t current_state = player.current_decoder().get_hash().hash();
+  const uint32_t initial_state = current_state;
+  deque<uint32_t> complete_states;
+  unordered_map<uint32_t, Decoder> decoders{{current_state, player.current_decoder()}};
+
+  /* memory usage logs */
+  system_clock::time_point next_mem_usage_report = system_clock::now();
+
+  
+
+  Poller poller;
+  
+  /*poller.add_action(Poller::Action(
+      udpsocket2, Direction::In,
+      [&]() {
+        const auto new_pcm = udpsocket2.recv(); //从网络上接收音频数据
+        
+        audio_queue.emplace(new_pcm.payload);
+        
+        return ResultType::Continue;
+      },
+      [&]() {return not udpsocket2.eof();}
+ ));*/
+ 
+  poller.add_action(Poller::Action(
+      udpsocket, Direction::In,
+      [&]() {
+        /* wait for next UDP datagram */
+        const auto new_fragment = udpsocket.recv(); //从网络上接收数据
+
+        /* parse into Packet */
+        const Packet packet{new_fragment.payload};
+
+        if (packet.frame_no() < next_frame_no)
+        {
+          /* we're not interested in this anymore */
+          return ResultType::Continue;
+        }
+        else if (packet.frame_no() > next_frame_no)
+        {
+          /* current frame is not finished yet, but we just received a packet
+           for the next frame, so here we just encode the partial frame and
+           display it and move on to the next frame */
+          cerr << "got a packet for frame #" << packet.frame_no()
+               << ", display previous frame(s)." << endl;
+
+          for (size_t i = next_frame_no; i < packet.frame_no(); i++)
+          {
+            if (fragmented_frames.count(i) == 0)
+              continue;
+
+            enqueue_frame(player, fragmented_frames.at(i).partial_frame());
+            fragmented_frames.erase(i);
+          }
+
+          next_frame_no = packet.frame_no();
+          current_state = player.current_decoder().minihash();
+        }
+
+        /* add to current frame */
+        if (fragmented_frames.count(packet.frame_no()))
+        {
+          fragmented_frames.at(packet.frame_no()).add_packet(packet);
+        }
+        else
+        {
+          /*
+          This was judged "too fancy" by the Code Review Board of Dec. 29, 2016.
+
+          fragmented_frames.emplace( std::piecewise_construct,
+                                     forward_as_tuple( packet.frame_no() ),
+                                     forward_as_tuple( connection_id, packet ) );
+        */
+
+          fragmented_frames.insert(make_pair(packet.frame_no(),
+                                             FragmentedFrame(connection_id, packet)));
+        }
+
+        /* is the next frame ready to be decoded? */
+        if (fragmented_frames.count(next_frame_no) > 0 and fragmented_frames.at(next_frame_no).complete())
+        {
+          auto &fragment = fragmented_frames.at(next_frame_no);
+
+          uint32_t expected_source_state = fragment.source_state();
+
+          if (current_state != expected_source_state)
+          {
+            if (decoders.count(expected_source_state))
+            {
+              /* we have this state! let's load it */
+              player.set_decoder(decoders.at(expected_source_state));
+              current_state = expected_source_state;
+            }
+          }
+
+          if (current_state == expected_source_state and
+              expected_source_state != initial_state)
+          {
+            /* sender won't refer to any decoder older than this, so let's get
+             rid of them */
+
+            auto it = complete_states.begin();
+
+            for (; it != complete_states.end(); it++)
+            {
+              if (*it != expected_source_state)
+              {
+                decoders.erase(*it);
+              }
+              else
+              {
+                break;
+              }
+            }
+
+            assert(it != complete_states.end());
+            complete_states.erase(complete_states.begin(), it);
+          }
+
+          // here we apply the frame
+          enqueue_frame(player, fragment.frame());
+
+          // state "after" applying the frame
+          current_state = player.current_decoder().minihash();
+
+          if (current_state == fragment.target_state() and
+              current_state != initial_state)
+          {
+            /* this is a full state. let's save it */
+            decoders.insert(make_pair(current_state, player.current_decoder()));
+            complete_states.push_back(current_state);
+          }
+
+          fragmented_frames.erase(next_frame_no);
+          next_frame_no++;
+        }
+
+        avg_delay.add(new_fragment.timestamp_us, packet.time_since_last());
+
+        AckPacket(connection_id, packet.frame_no(), packet.fragment_no(),
+                  avg_delay.int_value(), current_state,
+                  complete_states)
+            .sendto(udpsocket, new_fragment.source_address);
+
+        auto now = system_clock::now();
+
+        if (verbose and next_mem_usage_report < now)
+        {
+          cerr << "["
+               << duration_cast<milliseconds>(now.time_since_epoch()).count()
+               << "] "
+               << " <mem = " << procinfo::memory_usage() << ">\n";
+          next_mem_usage_report = now + 5s;
+        }
+
+        return ResultType::Continue;
+      },
+      [&]() { return not udpsocket.eof(); }
+  ));
+
+  /*poller.add_action(Poller::Action(
+    udpsocket2, Direction::In, 
+    [&]() {
+      // wait for next UDP datagram 
+        const auto new_pcm = udpsocket2.recv(); //从网络上接收音频数据
+
+        char * recv_buff = new char [new_pcm.payload.length()+1];
+        strcpy (recv_buff, new_pcm.payload.c_str());
+      //音频解码
+
+      //播放
+      if ((pcm = snd_pcm_writei(pcm_handle, recv_buff, audio_frames) == -EPIPE)){
+          printf("XRUN.\n");
+          snd_pcm_prepare(pcm_handle);
+        }
+
+    },
+    [&]() {return not udpsocket2.eof();}
+  ));
+  */
+  
+ 
+
+
+  
+  /* handle events */
+  while (true)
+  {
+    const auto poll_result = poller.poll(-1);
+    if (poll_result.result == Poller::Result::Type::Exit)
+    {
+      return poll_result.exit_status;
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
+
